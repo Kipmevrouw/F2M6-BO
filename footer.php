0452630bdefa4dfc3f4963209becbbf0d138a3ec
DannyvdZwart <32216@ma-web.nl>
2022-01-25 16:09:20 +0100

    footer items

---
+++ footer.php
@@ -0,0 +1,54 @@
+<footer>
+    <div1 id="d1">
+        <p id="dnk">
+            dankjewel,
+        </p>
+    </div1>
+<section id="plaatjes">
+<a target="_blank" href="https://www.facebook.com/FoamAmsterdam/" id="facebook">
+    <img src="<?php echo get_template_directory_uri()?>/icons/facebook.png"/>
+    </a>
+    <a target="_blank" href="https://twitter.com/foam_amsterdam" id="twitter">
+    <img src="<?php echo get_template_directory_uri()?>/icons/twitter.png"/>
+    </a>
+    <a target="_blank" href="https://www.youtube.com/c/FoamOrg" id="youtube">
+    <img src="<?php echo get_template_directory_uri()?>/icons/youtube.png"/>
+    </a>
+    <a target="_blank" href="https://www.instagram.com/foam_amsterdam/?hl=nl" id="insta">
+    <img src="<?php echo get_template_directory_uri()?>/icons/insta.png"/>
+    </a>
+    <a target="_blank" href="https://www.tiktok.com/@foamamsterdam" id="tiktok">
+    <img src="<?php echo get_template_directory_uri()?>/icons/tiktok.png"/>
+    </a>
+</section>
+    <div2 id="d2">
+        <p id="nws">
+            nieuwsbrief
+        </p>
+        <img src="<?php echo get_template_directory_uri()?>/icons/mail.png"/>
+    </div2>
+    <div3 id="d3">
+        <p id="bzk">
+            Bezoek ons
+        </p>
+        <img src="<?php echo get_template_directory_uri()?>/icons/pijl1.png"/>
+    </div3>
+    <div4 id="d4">
+        <p id="nl">
+            nl
+        </p>
+        <img src="<?php echo get_template_directory_uri()?>/icons/globe.png"/>
+    </div4>
+    <div5 id="d5">
+        <p id="">
+            privacybeleid
+        </p>
+    </div5>    
+    <div6 id="d6">
+        <p id="dnk">
+            ©2021 Foam
+        </p>
+    </div6>
+
+
+</footer>
